7556ee6d881bcf0bd4281a090f75d8bc78b02ac3
russell <duugr@tuta.io>
2022-09-05 18:27:37 +0800

    order

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -10,17 +10,18 @@ import (
 type (
 	Order struct {
 		gorm.Model
-		OrderNumber  string `json:"OrderNumber" db:"order_number"`
-		ProductId    string `json:"ProductId" db:"product_id"`
-		UserId       string `json:"UserId" db:"user_id"`
-		ExternalId   string `json:"ExternalId" db:"external_id"`
-		Channel      string `json:"Channel" db:"channel"`
-		Queue        string `json:"Queue" db:"queue"`
-		State        string `json:"State" db:"state"`
-		CardNumber   string `json:"CardNumber" db:"card_number"`
-		CardPassword string `json:"CardPassword" db:"card_password"`
-		CardCvv      string `json:"CardCvv" db:"card_cvv"`
-		Result       string `json:"Result" db:"result"`
+		OrderNumber  string       `json:"OrderNumber" db:"order_number"`
+		ProductId    string       `json:"ProductId" db:"product_id"`
+		UserId       string       `json:"UserId" db:"user_id"`
+		ExternalId   string       `json:"ExternalId" db:"external_id"`
+		Channel      string       `json:"Channel" db:"channel"`
+		Queue        string       `json:"Queue" db:"queue"`
+		State        string       `json:"State" db:"state"`
+		CardNumber   string       `json:"CardNumber" db:"card_number"`
+		CardPassword string       `json:"CardPassword" db:"card_password"`
+		CardCvv      string       `json:"CardCvv" db:"card_cvv"`
+		ResultCard   gorm.JsonAny `json:"ResultCard" db:"result_card"`
+		ResultPay    gorm.JsonAny `json:"ResultPay" db:"result_pay"`
 	}
 	_order struct{}
 )
@@ -31,6 +32,66 @@ func (*_order) db() *gdb.DB {
 	return gorm.NewModel(&Order{}).Where("deleted_at IS NULL")
 }
 
+func (*_order) ChannelPC() string {
+	return "web.pc"
+}
+func (*_order) ChannelMobile() string {
+	return "web.mobile"
+}
+func (*_order) ChannelApi() string {
+	return "api"
+}
+
+// 创建订单默认状态
+func (*_order) QueueCardQueue() string {
+	return "card.queue"
+}
+
+// 订单被调用, 发起销卡
+func (*_order) QueueCardRequest() string {
+	return "card.request"
+}
+
+// 销卡成功
+func (*_order) QueueCardSuccess() string {
+	return "card.success"
+}
+
+// 销卡失败
+func (*_order) QueueCardError() string {
+	return "card.error"
+}
+
+// 销卡成功后, 发起付款
+func (*_order) QueuePayRequest() string {
+	return "pay.request"
+}
+
+// 付款成功
+func (*_order) QueuePaySuccess() string {
+	return "pay.success"
+}
+
+// 付款失败
+func (*_order) QueuePayError() string {
+	return "pay.error"
+}
+
+// 订单执行中
+func (*_order) StateHang() string {
+	return "hang"
+}
+
+// 订单执行结束, 成功完成
+func (*_order) StateSuccess() string {
+	return "success"
+}
+
+// 订单失败
+func (*_order) StateError() string {
+	return "error"
+}
+
 func (o *_order) GetOrder(orderId string) (*Order, error) {
 	var order Order
 	err := o.db().Where("nanoid = ?", orderId).First(&order).Error
@@ -38,6 +99,14 @@ func (o *_order) GetOrder(orderId string) (*Order, error) {
 		zaplog.Sugar.Error(err)
 	}
 	return &order, err
+}
+
+func (e *_order) GetQueueOrders(offset, limit int) (orders []*Order, count int64, err error) {
+	err = e.db().Where("state=?", e.StateHang()).Count(&count).Order("id desc").Limit(limit).Offset(offset).Find(&orders).Error
+	if err != nil {
+		zaplog.Sugar.Error(err)
+	}
+	return
 }
 
 func (e *_order) GetOrders(offset, limit int) (orders []*Order, count int64, err error) {
--- internal/logic/order.go
+++ internal/logic/order.go
@@ -16,7 +16,7 @@ func (l *_order) List(page Page) (result Result, err error) {
 
 	data, count, err := entity.OrderEntity.GetOrders(page.getOffset(), page.getLimit())
 	if len(data) <= 0 || err != nil {
-		err = errors.New("用户认证无数据")
+		err = errors.New("订单无数据")
 		return
 	}
 
@@ -30,30 +30,30 @@ func (l *_order) List(page Page) (result Result, err error) {
 func (l *_order) Create(data entity.Order) error {
 	err := entity.OrderEntity.Insert(&data)
 	if data.ID <= 0 || err != nil {
-		return errors.New("新增用户认证失败")
+		return errors.New("新增订单失败")
 	}
 
-	l.Log("新增用户认证", data)
+	l.Log("新增订单", data)
 	return err
 }
 func (l *_order) Update(data entity.Order) (err error) {
 	err = entity.OrderEntity.Delete(data.ID)
 	if err != nil {
-		return errors.New("更新用户认证失败")
+		return errors.New("更新订单失败")
 	}
 	err = entity.OrderEntity.Insert(&data)
 	if data.ID <= 0 || err != nil {
-		return errors.New("更新用户认证失败")
+		return errors.New("更新订单失败")
 	}
 
-	l.Log("更新用户认证", data)
+	l.Log("更新订单", data)
 	return err
 }
 func (l *_order) Delete(userId int) error {
 	err := entity.OrderEntity.Delete(userId)
 	if err != nil {
-		return errors.New("删除用户认证失败")
+		return errors.New("删除订单失败")
 	}
-	l.Log("删除用户认证", userId)
+	l.Log("删除订单", userId)
 	return err
 }
